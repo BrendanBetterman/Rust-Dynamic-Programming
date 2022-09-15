89c982293f5e385802697eeeae8a5e01fed6749b
On0n0k1 <stiltztinkerstein@gmail.com>
2022-09-15 09:51:08 -0300

    Adding fib2 with a new implementation.

---
+++ fib2/target/debug/deps/fib2-4813bfce913e193a.d
@@ -0,0 +1,5 @@
+/home/clarund/Projects/delete_later/delete/Rust-Dynamic-Programming/fib2/target/debug/deps/fib2-4813bfce913e193a.rmeta: src/main.rs
+
+/home/clarund/Projects/delete_later/delete/Rust-Dynamic-Programming/fib2/target/debug/deps/fib2-4813bfce913e193a.d: src/main.rs
+
+src/main.rs:
---
+++ fib2/target/debug/deps/fib2-ff98a6cb74f9ac84.d
@@ -0,0 +1,5 @@
+/home/clarund/Projects/delete_later/delete/Rust-Dynamic-Programming/fib2/target/debug/deps/fib2-ff98a6cb74f9ac84.rmeta: src/main.rs
+
+/home/clarund/Projects/delete_later/delete/Rust-Dynamic-Programming/fib2/target/debug/deps/fib2-ff98a6cb74f9ac84.d: src/main.rs
+
+src/main.rs:
